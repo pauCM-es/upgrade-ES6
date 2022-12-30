b5e1f0a920f6b6262e6701232f6262e88309dd35
pauCM-es <costamingarro@gmail.com>
2022-12-30 11:41:37 +0100

    iteration 2 - destructuring

--- index.js
+++ index.js
@@ -39,6 +39,9 @@ console.log(title, gender, year)
 
 const fruits = ["Banana", "Strawberry", "Orange"];
 
+const [fruit1, fruit2, fruit3] = fruits
+console.log(fruit1, fruit2, fruit3)
+
 // 2.3 En base al siguiente javascript, usa destructuring para crear 2
 // variables igualandolo a la función e imprimiendolo por consola.
 
@@ -46,9 +49,19 @@ const animalFunction = () => {
   return { name: "Bengal Tiger", race: "Tiger" };
 };
 
+const {name, race} = animalFunction()
+console.log(name)
+console.log(race)
+
 // 2.4 En base al siguiente javascript, usa destructuring para crear las
 // variables name y itv con sus respectivos valores. Posteriormente crea
 // 3 variables usando igualmente el destructuring para cada uno de los años
 // y comprueba que todo esta bien imprimiendolo.
 
-const car = { name: "Mazda 6", itv: [2015, 2011, 2020] };
+const car = { name1: "Mazda 6", itv: [2015, 2011, 2020] };
+
+const {name1, itv} = car
+const [year1, year2, year3] = itv
+
+console.log(name1, itv)
+console.log(year1, year2, year3)
