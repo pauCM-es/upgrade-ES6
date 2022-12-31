92b9cb54d14156735a4f89bf40df351675c9b6a0
pauCM-es <costamingarro@gmail.com>
2022-12-31 11:47:20 +0100

    iteration 7 - reduce

--- index.js
+++ index.js
@@ -295,3 +295,52 @@ const alienFound = aliens.find(alien => alien.name === "Cucushumushu")
 const mutationFound = mutations.find(mutation => mutation.name === "Porompompero")
 const mutants = {...alienFound, mutation: {...mutationFound}}
 console.log(mutants)
+
+//*Iteración #7: Reduce
+console.log("----Iteración #7: Reduce----")
+
+// 7.1 Dado el siguiente array, haz una suma de todos las notas de los examenes de 
+// los alumnos usando la función .reduce().
+
+const exams = [
+    {name: 'Yuyu Cabeza Crack', score: 5}, 
+    {name: 'Maria Aranda Jimenez', score: 1}, 
+    {name: 'Cristóbal Martínez Lorenzo', score: 6}, 
+    {name: 'Mercedez Regrera Brito', score: 7},
+    {name: 'Pamela Anderson', score: 3},
+    {name: 'Enrique Perez Lijó', score: 6},
+    {name: 'Pedro Benitez Pacheco', score: 8},
+    {name: 'Ayumi Hamasaki', score: 4},
+    {name: 'Robert Kiyosaki', score: 2},
+    {name: 'Keanu Reeves', score: 10}
+];
+
+const totalScore = exams.reduce(
+	(accumulator, currentValue) => accumulator + currentValue.score,
+	0
+)
+console.log(totalScore)
+
+// 7.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los 
+// alumnos que esten aprobados usando la función .reduce().
+
+const totalScore2 = exams.reduce(
+	(accumulator, currentExam) => {
+		if (currentExam.score >= 5) {
+			return accumulator + currentExam.score
+		} else {
+			return accumulator
+		}
+	}, 0
+)
+console.log(totalScore2)
+
+// 7.3 Dado el mismo array, haz la media de las notas de todos los examenes .reduce().
+let people = 0
+const averageScore = exams.reduce(
+	(accumulator, currentExam) => {
+		people++
+		return accumulator + currentExam.score
+	}, 0
+) / people
+console.log(averageScore)
