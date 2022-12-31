f98e103a706519a9fc9d4a0abef4371fc24c704b
pauCM-es <costamingarro@gmail.com>
2022-12-31 09:49:40 +0100

    iteration 4 - map

--- index.js
+++ index.js
@@ -107,3 +107,58 @@ const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
 const colorsCopy = [...colors]
 colorsCopy.splice(1,1)
 console.log(colorsCopy)
+
+//* Iteración #4: Map
+console.log("----Iteración #4: Map----")
+
+
+// 4.1 Dado el siguiente array, devuelve un array con sus nombres 
+// utilizando .map().
+const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+const usersNames = users.map( user => {return user.name})
+console.log(usersNames)
+
+// 4.2 Dado el siguiente array, devuelve una lista que contenga los valores 
+// de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
+// empiece por 'A'.
+const users2 = [
+	{id2: 1, name2: 'Abel'},
+	{id2:2, name2: 'Julia'},
+	{id2:3, name2: 'Pedro'},
+	{id2:4, name2: 'Amanda'}
+];
+
+const users2List = users2.map( user => {
+	if (user.name2[0] === "A") {
+		return "Anacleto"
+	} else {
+		return user.name2
+	}
+})
+console.log(users2List)
+
+// 4.3 Dado el siguiente array, devuelve una lista que contenga los valores 
+// de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
+// cuando el valor de la propiedad isVisited = true.
+const cities = [
+	{isVisited:true, cityName: 'Tokyo'}, 
+	{isVisited:false, cityName: 'Madagascar'},
+	{isVisited:true, cityName: 'Amsterdam'}, 
+	{isVisited:false, cityName: 'Seul'}
+];
+
+const citiesVisited = cities.map (city => {
+	if (city.isVisited === true ) {
+		return `${city.cityName} (Visited)` 
+	} else {
+		return city.cityName
+	}
+})
+
+console.log(citiesVisited)
