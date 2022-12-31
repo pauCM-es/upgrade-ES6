e03af6751b40101233e4d809a0e3755f0d038115
pauCM-es <costamingarro@gmail.com>
2022-12-31 11:55:03 +0100

    iteration 8 - bonus

--- index.js
+++ index.js
@@ -344,3 +344,31 @@ const averageScore = exams.reduce(
 	}, 0
 ) / people
 console.log(averageScore)
+
+//*Iteración #8: Bonus
+console.log("----Iteración #8: Bonus----")
+
+// 6.1 Dado el siguiente javascript filtra los videojuegos por gender = 'RPG' usando 
+// .filter() y usa .reduce() para conseguir la media de sus .score. 
+// La función .find() también podría ayudarte para el contrar el genero 'RPG' en el 
+// array .gender.
+
+const videogames = [
+    {name: 'Final Fantasy VII', genders: ['RPG'], score: 9.5},
+    {name: 'Assasins Creed Valhala', genders: ['Aventura', 'RPG'], score: 4.5},
+    {name: 'The last of Us 2', genders: ['Acción', 'Aventura'], score: 9.8},
+    {name: 'Super Mario Bros', genders: ['Plataforma'], score: 8.5},
+    {name: 'Genshin Impact', genders: ['RPG', 'Aventura'], score: 7.5},
+    {name: 'Legend of Zelda: Breath of the wild', genders: ['RPG', 'La cosa más puto bonita que he visto nunca'], score: 10},
+]
+
+let gameCount = 0
+const rpgGamesScore = videogames.filter( game => {
+	return game.genders.find( gender => gender === "RPG")
+}).reduce(
+	(accum, curGame) => {
+		gameCount++
+		return accum + curGame.score
+	}, 0
+) / gameCount
+console.log(rpgGamesScore)
